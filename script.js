178d7334b6b69c959920113ac3d64704f29b0bcf
sanjusahu2002 <132829320+sanjusahu2002@users.noreply.github.com>
2024-11-12 01:39:48 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-function getFormvalue() {
-    //Write your code here
-
+function getFormvalue(){
+            let f2 = document.getElementById("fname");
+            let l2 = document.getElementById("lname"); 
+            alert(f2.value+" "+l2.value);
 }
